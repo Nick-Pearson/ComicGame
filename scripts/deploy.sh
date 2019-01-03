5658fed24d903637e08d34b05c71f3347889640f
Nick <nicholas.pearson4@btinternet.com>
2019-01-03 00:16:31 +0000

    Refactored to use after success

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "JWT_SECRET=$PROD_JWT_SECRET" > .env
-echo "PORT=80" > .env
-docker build -t nickpearson/comicgame .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push nickpearson/comicgame:latest
