2686d7755980314694a1f478467a509e68982f58
Nick <nicholas.pearson4@btinternet.com>
2019-01-04 16:17:17 +0000

    Fix travis .env file, kubernetes deployment and port in env

--- TODO.md
+++ TODO.md
@@ -4,7 +4,7 @@ TODO:
 * Final scoreboard
 * Setup MongoDB and write python wrapper
 * Setup volume storage and python wrapper
-* Setup docker and CI creating docker file
+* ~Setup docker and CI creating docker file~
 * Configure kubernetes and a load balancer
 * Write stress test scripts
 If time!
--- settings.py
+++ settings.py
@@ -7,4 +7,9 @@ __settings = dotenv_values(dotenv_path = "./.env", verbose=True);
 if "JWT_SECRET" in __settings:
     JWT_KEY = __settings["JWT_SECRET"];
 else:
-    raise RuntimeError("Invalid or missing JWT secret");
+    raise RuntimeError("Missing JWT secret");
+
+if "PORT" in __settings:
+    PORT = __settings["PORT"];
+else:
+    PORT = 5000
