74979123be3002ce870dee0f387628afc4527cbe
Nick <nicholas.pearson4@btinternet.com>
2019-01-11 15:25:19 +0000

    Validate config before use

--- imagestore/object_imagestore.py
+++ imagestore/object_imagestore.py
@@ -24,7 +24,7 @@ class ObjectImageStore(ImageStoreBase):
                 'key_file': '/root/.oci/oci_api_key.pem'
             };
 
-        print(config);
+        oci.validate_config(config);
         this.obj = oci.object_storage.ObjectStorageClient(config);
 
     ######################
